da7b8b4f27f877e74ed6698c269b76bb01bd7018
Atul Singh <atulsingh05540@gmail.com>
2026-01-07 17:25:11 +0530

    basic array question from today

---
+++ src/week3QuestionsPractice/q2.java
@@ -0,0 +1,15 @@
+package week3QuestionsPractice;
+import java.util.Arrays;
+import java.util.Scanner;
+public class q2 {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20};
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+        for (int i = 1; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
