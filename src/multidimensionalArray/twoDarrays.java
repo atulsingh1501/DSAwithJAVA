e3e7330375daa018d01f93f2671e2a1d0580c9a3
Atul Singh <atulsingh05540@gmail.com>
2026-01-15 21:48:56 +0530

    mutidimensional array q1

--- src/multidimensionalArray/twoDarrays.java
+++ src/multidimensionalArray/twoDarrays.java
@@ -1,7 +1,29 @@
 package multidimensionalArray;
-
+import java.util.Arrays;
 public class twoDarrays {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = arr.length;        // number of rows
+        int m = arr[0].length;     // number of columns
+
+        for (int i = 0; i < n; i++) {     // loop for rows
+            int sum = 0;                  // reset sum for each row
+
+            for (int j = 0; j < m; j++) { // loop for columns
+                sum = sum + arr[i][j];
+            }
+
+            System.out.println("Row " + (i + 1) + " sum = " + sum);
+        }
+    }
 }
+
 //What is a Multidimensional Array?
 //
 //A multidimensional array is basically an array of arrays.
