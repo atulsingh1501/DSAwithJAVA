33dda1eec507ad099681148bfcd5224ad926d74e
Atul Singh <atulsingh05540@gmail.com>
2026-01-18 17:24:27 +0530

    basics revision

---
+++ src/week4QuestionsPractice/q6.java
@@ -0,0 +1,16 @@
+package week4QuestionsPractice;
+import java.util.Scanner;
+public class q6 {
+    public static void main(String[] args) {
+//        Print even numbers from 1 to N.
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+//        for(int i = 1; i <= n; i++){
+//            if(i % 2 == 0){
+//                System.out.println(i);
+//            }
+        for (int i = 2; i <= n; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
