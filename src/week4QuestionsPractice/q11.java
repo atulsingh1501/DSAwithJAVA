6b173d75733f576ff3120382d0d6007c1af9452a
Atul Singh <atulsingh05540@gmail.com>
2026-01-18 18:30:26 +0530

    basics revision of conditinal

---
+++ src/week4QuestionsPractice/q11.java
@@ -0,0 +1,15 @@
+package week4QuestionsPractice;
+import java.util.Scanner;
+public class q11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        sc.close();
+    }
+}
