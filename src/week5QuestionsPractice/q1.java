fc2eee8e71c1fc680bcf8f644951d39ffee48329
Atul Singh <atulsingh05540@gmail.com>
2026-01-20 22:31:49 +0530

    basics revision of array

---
+++ src/week5QuestionsPractice/q1.java
@@ -0,0 +1,21 @@
+package week5QuestionsPractice;
+
+public class q1 {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 6, 7};
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Even numbers count: " + count);
+    }
+}
+//Question: Count Even Numbers in an Array
+//
+//Problem statement:
+//Given an integer array arr, count how many even numbers are present in it.
