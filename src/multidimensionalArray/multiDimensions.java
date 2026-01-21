de36a1931d7e7f0e1316e2cf206d9b618d88e38b
Atul Singh <atulsingh05540@gmail.com>
2026-01-21 07:01:19 +0530

    2d array

---
+++ src/multidimensionalArray/multiDimensions.java
@@ -0,0 +1,21 @@
+package multidimensionalArray;
+
+public class multiDimensions {
+    public static void main(String[] args) {
+/*
+      1 2 3
+      4 5 6
+      7 8 9
+*/
+        int[][] arr = new int[3][];
+
+        int[][] arr2D = { /* indivisual array have a dofferent size */
+                {1,2,3},//0th index
+                {4,5},//1st index
+                {6,7,8,9}//2nd index-> arr2D[2][0] = {6,7,8,9}
+        };
+        //input
+
+    }
+
+}
