17fdee81c23f75e72fcda3d7f1b343683d9042d1
Atul Singh <atulsingh05540@gmail.com>
2026-01-23 20:37:09 +0530

    swap array element

---
+++ src/week5QuestionsPractice/q4_largestElement.java
@@ -0,0 +1,18 @@
+package week5QuestionsPractice;
+
+public class q4_largestElement {
+//    Given an integer array, find the largest element present in the array.
+public static void main(String[] args) {
+    int[] arr = {3, 7, 1, 9, 4};
+
+    int max = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    System.out.println("Largest element: " + max);
+}
+}
