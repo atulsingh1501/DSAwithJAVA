b7011c2e844056311fa1149fbc7369e3c64587a5
Atul Singh <atulsingh05540@gmail.com>
2026-02-01 11:36:22 +0530

    linearSearch finding min element

--- src/week7QuestionsPractice/q3_minElement.java
+++ src/week7QuestionsPractice/q3_minElement.java
@@ -1,6 +1,6 @@
 public class q3_minElement {
     public static void main(String[] args) {
-        int [] arr = {18,12,3,14,28};
+        int [] arr = {18,12,-7,3,14,28};
 
         int min = arr[0];// correct way
 
