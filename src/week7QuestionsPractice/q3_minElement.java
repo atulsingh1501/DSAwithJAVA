d43b04f8231525b7b74b0f15db45e040f4b2b5ae
Atul Singh <atulsingh05540@gmail.com>
2026-02-01 11:28:39 +0530

    linearSearch

--- src/week7QuestionsPractice/q3_minElement.java
+++ src/week7QuestionsPractice/q3_minElement.java
@@ -1,15 +1,15 @@
-package week7QuestionsPractice;
-
 public class q3_minElement {
     public static void main(String[] args) {
         int [] arr = {18,12,3,14,28};
-        int min = 28;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i]<min){
-                min = arr[i];
 
+        int min = arr[0];// correct way
+
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
             }
         }
-        System.out.println("Minimum element is "+min);
+
+        System.out.println("Minimum element is " + min);
     }
 }
