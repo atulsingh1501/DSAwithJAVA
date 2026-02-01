1d382bd5573f8b9a54d5d043bc03fda24655ce67
Atul Singh <atulsingh05540@gmail.com>
2026-02-01 12:01:17 +0530

    Searching in 2d array

---
+++ src/linearSearch/searchIn2DArray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class searchIn2DArray {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {23,4,1},
+                {18,12,3,9},
+                {78,99,34,56},
+                {18,0}
+        };
+
+        int target = 56;
+
+        int[] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] search(int[][] arr, int target){
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+
+                if(arr[i][j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
--- src/week7QuestionsPractice/q3_minElement.java
+++ src/week7QuestionsPractice/q3_minElement.java
@@ -1,3 +1,4 @@
+package week7QuestionsPractice;
 public class q3_minElement {
     public static void main(String[] args) {
         int [] arr = {18,12,-7,3,14,28};
