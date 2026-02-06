9efc321be667ad5a40f8ad35e871f41cbdd58dda
Atul Singh <atulsingh05540@gmail.com>
2026-02-06 23:13:56 +0530

    binary search

---
+++ src/BinarySearch/binarySearch.java
@@ -0,0 +1,13 @@
+package BinarySearch;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int [] arr = {2,4,6,8,10,12,14,16,18,20};
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            int mid = (start+end)/2;
+        }
+    }
+}
