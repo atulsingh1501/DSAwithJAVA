c581aac9de354fde61819165ebbcc4d471cedbd9
Atul Singh <atulsingh05540@gmail.com>
2026-02-06 23:16:51 +0530

    binary search

--- src/BinarySearch/binarySearch.java
+++ src/BinarySearch/binarySearch.java
@@ -2,12 +2,28 @@ package BinarySearch;
 
 public class binarySearch {
     public static void main(String[] args) {
-        int [] arr = {2,4,6,8,10,12,14,16,18,20};
-        int start = 0;
-        int end = arr.length-1;
 
-        while(start<=end){
-            int mid = (start+end)/2;
+        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int target = 14;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(arr[mid] == target){
+                System.out.println("Found at index: " + mid);
+                return;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
         }
+
+        System.out.println("Not found");
     }
 }
