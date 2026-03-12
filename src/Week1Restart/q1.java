33a0931d51d71922fa7ea0413a9647d407ef3a23
Atul Singh <atulsingh05540@gmail.com>
2026-03-12 17:16:26 +0530

    Variables & Basic Syntax after a big gaps

---
+++ src/Week1Restart/q1.java
@@ -0,0 +1,13 @@
+package Week1Restart;
+
+public class q1 {
+    public static void main(String[] args) {
+        String name = "Atul";
+        int age = 21;
+        double height = 5.6;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
