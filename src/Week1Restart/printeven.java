69eb767682e79c70203b03165430dd337a9b5d32
Atul Singh <atulsingh05540@gmail.com>
2026-03-13 12:02:18 +0530

    basic loop revsion

---
+++ src/Week1Restart/printeven.java
@@ -0,0 +1,16 @@
+package Week1Restart;
+
+public class printeven {
+    public static void main(String[] args) {
+//        int n = 50;
+//        for (int i = 1; i <= n; i++) {
+//            if(i%2==0){
+//                System.out.println(i);
+//            }
+        for(int i = 2; i <= 50; i = i + 2){
+            System.out.println(i);
+        }
+        }
+    }
+
+
